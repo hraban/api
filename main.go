c2f7c5c990643252ba27c97e41690eb5692e4853
Hraban Luyat <hraban@0brg.net>
2016-12-29 01:11:57 +0000

    Respect this program pls

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+func yesliekttly(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/plain; charset=utf-8; brains=tasty")
+	fmt.Fprintf(w, "yes this program is basically the best in the world.")
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -15,5 +20,6 @@ func main() {
 	}
 
 	log.Printf("Listening on %s", port)
+	http.HandleFunc("/isthisprogramawesome", yesliekttly)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
